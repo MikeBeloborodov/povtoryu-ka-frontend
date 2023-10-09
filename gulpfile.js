71dea70a31827c4e0248e63f72523a01058d9b09
Mikhail Beloborodov <kiritohenjanai@gmail.com>
2023-10-10 01:07:27 +0400

    added fonts

--- gulpfile.js
+++ gulpfile.js
@@ -10,8 +10,8 @@ const cssnano = require('cssnano');
 const htmlMinify = require('html-minifier');
 const jsMinify = require('gulp-minify');
 const sass = require('gulp-sass')(require('sass'));
-const ts = require("gulp-typescript");
-const tsProject = ts.createProject("tsconfig.json");
+const ts = require('gulp-typescript');
+const tsProject = ts.createProject('tsconfig.json');
 
 function html() {
   const options = {
@@ -23,58 +23,71 @@ function html() {
     useShortDoctype: true,
     collapseWhitespace: true,
     minifyCSS: true,
-    keepClosingSlash: true
+    keepClosingSlash: true,
   };
-  return gulp.src('src/**/*.html')
+  return gulp
+    .src('src/**/*.html')
     .pipe(plumber())
     .on('data', function (file) {
-      const bufferFile = Buffer.from(htmlMinify.minify(file.contents.toString(), options))
-      return file.contents = bufferFile
+      const bufferFile = Buffer.from(
+        htmlMinify.minify(file.contents.toString(), options)
+      );
+      return (file.contents = bufferFile);
     })
     .pipe(gulp.dest('dist/'))
     .pipe(browserSync.reload({ stream: true }));
 }
 
 function scss() {
-  const plugins = [
-    autoprefixer(),
-    media_query(),
-    cssnano()
-  ];
-  return gulp.src('src/styles/**/*.scss')
+  const plugins = [autoprefixer(), media_query(), cssnano()];
+  return gulp
+    .src('src/**/*.scss')
     .pipe(sass())
     .pipe(concat('bundle.css'))
     .pipe(postcss(plugins))
     .pipe(gulp.dest('dist/'))
-    .pipe(browserSync.reload({ stream: true }))
+    .pipe(browserSync.reload({ stream: true }));
 }
 
 function images() {
-  return gulp.src('src/images/**/*.{jpg,png,gif,ico,webp,avif}')
+  return gulp
+    .src('src/images/**/*.{jpg,png,gif,ico,webp,avif}')
     .pipe(gulp.dest('dist/images'))
     .pipe(browserSync.reload({ stream: true }));
 }
 
+function favicons() {
+  return gulp
+    .src('src/favicons/**/*.{jpg,png,gif,ico,webp,avif,svg}')
+    .pipe(gulp.dest('dist/favicons'))
+    .pipe(browserSync.reload({ stream: true }));
+}
+
 function svg() {
-  return gulp.src('src/svg/**/*.svg')
+  return gulp
+    .src('src/svg/**/*.svg')
     .pipe(gulp.dest('dist/svg'))
-    .pipe(browserSync.reload({stream: true}));
+    .pipe(browserSync.reload({ stream: true }));
 }
 
 function type_script() {
-  return  tsProject.src()
-    .pipe(tsProject()).js
-    .pipe(gulp.dest('dist/'));
+  return tsProject
+    .src()
+    .pipe(tsProject())
+    .js.pipe(gulp.dest('dist/'))
+    .pipe(browserSync.reload({ stream: true }));
 }
 
 function fonts() {
-  return gulp.src('src/fonts/**/*.{ttf,woff,woff2}')
+  return gulp
+    .src('src/fonts/**/*.{ttf,woff,woff2}')
     .pipe(gulp.dest('dist/fonts'))
     .pipe(browserSync.reload({ stream: true }));
 }
 
 function videos() {
-  return gulp.src('src/videos/**/*.{avi,mp4,webm}')
+  return gulp
+    .src('src/videos/**/*.{avi,mp4,webm}')
     .pipe(gulp.dest('dist/videos'))
     .pipe(browserSync.reload({ stream: true }));
 }
@@ -85,30 +98,38 @@ function clean() {
 
 function watchFiles() {
   gulp.watch(['src/**/*.html'], html);
-  gulp.watch(['src/styles/**/*.scss'], scss);
+  gulp.watch(['src/**/*.scss'], scss);
   gulp.watch(['src/images/**/*.{jpg,png,svg,gif,ico,webp,avif}'], images);
+  gulp.watch(
+    ['src/favicons/**/*.{jpg,png,svg,gif,ico,webp,avif,svg}'],
+    favicons
+  );
   gulp.watch(['src/svg/**/*.svg'], svg);
   gulp.watch(['src/fonts/**/*.{ttf,woff,woff2}'], fonts);
   gulp.watch(['src/videos/**/*.{avi,mp4,webm}'], videos);
-  gulp.watch(['src/scripts/**/*.{ts}'], type_script);
+  gulp.watch(['src/scripts/**/*.ts'], type_script);
 }
 
 function serve() {
   browserSync.init({
     server: {
-      baseDir: './dist'
-    }
+      baseDir: './dist',
+    },
   });
 }
 
 // add css if needed
-const build = gulp.series(clean, gulp.parallel(html, scss, images, svg, fonts, videos, type_script));
+const build = gulp.series(
+  clean,
+  gulp.parallel(html, scss, images, favicons, svg, fonts, videos, type_script)
+);
 const watchapp = gulp.parallel(build, watchFiles, serve);
 
 exports.html = html;
 // exports.css = css;
 exports.scss = scss;
 exports.images = images;
+exports.favicons = favicons;
 exports.svg = svg;
 exports.fonts = fonts;
 exports.videos = videos;
--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -1,1 +1,1 @@
-console.log("Hello, world!")
+console.log("new thingy")
